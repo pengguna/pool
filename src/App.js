011de78f870d80a7cfaccd9b1727c5728b38c96b
me <me@mes-MacBook-Air.local>
2022-06-15 19:32:16 +1000

    mvp push button do req

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Pool Game </h1>
+      < NewGameCollector />
       </header>
     </div>
   );
+}
+
+function NewGameCollector () {
+
+    const [winner, setWinner] = useState('');
+    const [loser, setLoser] = useState('');
+    const handlePost = () => {
+        const requestOptions = {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*', 
+            },
+            body: JSON.stringify({ winner, loser,}),
+        };
+        fetch("http://localhost:8043", requestOptions)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    };
+    return (
+        <Stack spacing={2} direction="row">
+            <TextField id="outlined-basic" label="Winner" variant="outlined" value={winner} onChange={(e) => {setWinner(e.target.value)}} />
+            <TextField id="outlined-basic" label="Loser" variant="outlined" value={loser} onChange={(e) => {setLoser(e.target.value)}} />
+            <Button variant="outlined" onClick={handlePost}>Submit</Button>
+        </Stack>
+    )
 }
 
 export default App;
